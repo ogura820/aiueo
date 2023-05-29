e0be69c224fc68a96a6bca69f054216b1072a129
ryo ono <ryo.ono@gmail.com>
2023-05-29 20:08:58 +0900

    Create_Real_estate's_Table

--- db/migrate/20230529110136_create_real_estates.rb
+++ db/migrate/20230529110136_create_real_estates.rb
@@ -1,7 +1,11 @@
 class CreateRealEstates < ActiveRecord::Migration[6.0]
   def change
     create_table :real_estates do |t|
-
+      t.string :name
+      t.integer :rent
+      t.text :address
+      t.date :age
+      t.integer :note
       t.timestamps
     end
   end
