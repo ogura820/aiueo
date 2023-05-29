e09c71c09a88b71375b23ad8ebd316f288191110
ryo ono <ryo.ono@gmail.com>
2023-05-29 21:31:37 +0900

    Add_新規投稿

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -1,2 +1,20 @@
 class RealEstatesController < ApplicationController
+  def index
+  end
+
+  def new
+    @real_estates = RealEstate.new
+  end
+
+  def create
+    RealEstate.create(real_estate_params)
+    redirect_to real_estates_path
+  end
+
+  private
+
+  def real_estate_params
+    params.require(:real_estate).permit(:name, :rent, :address, :age, :note)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root 'real_estates#index'
   resources :real_estates
 end
