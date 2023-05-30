faca629e4520fff9c6c07c9fc57bb69c1c2427c0
ryo ono <ryo.ono@gmail.com>
2023-05-30 14:03:15 +0900

    Add_物件削除機能

--- app/controllers/real_estates_controller.rb
+++ app/controllers/real_estates_controller.rb
@@ -1,9 +1,10 @@
 class RealEstatesController < ApplicationController
   def index
+    @real_estates = RealEstate.all
   end
 
   def new
-    @real_estates = RealEstate.new
+    @real_estate = RealEstate.new
   end
 
   def create
@@ -11,10 +12,36 @@ class RealEstatesController < ApplicationController
     redirect_to real_estates_path
   end
 
+  def show
+    set_real_estate
+  end
+
+  def edit
+    set_real_estate
+  end
+
+  def update
+    set_real_estate
+    if @real_estate.update(real_estate_params)
+      redirect_to real_estates_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    set_real_estate
+    @real_estate.destroy
+    redirect_to real_estates_path
+  end
+  
   private
 
   def real_estate_params
     params.require(:real_estate).permit(:name, :rent, :address, :age, :note)
   end
   
+  def set_real_estate
+    @real_estate = RealEstate.find(params[:id])
+  end
 end
