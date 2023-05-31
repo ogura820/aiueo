59035245df2174c1cf0ece7d9ee01696500ac8f6
ryo ono <ryo.ono@gmail.com>
2023-05-31 16:04:33 +0900

    Fix_デザイン(リンク色・送信ボタンの文面)

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,5 +1,5 @@
 class RealEstate < ApplicationRecord
-  has_many :nearest_stations,dependent: :destroy
+  has_many :nearest_stations
   accepts_nested_attributes_for :nearest_stations, 
                                   allow_destroy: true, 
                                   reject_if: :all_blank
