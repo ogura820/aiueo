798a7108b50c54016dd800ac21d72ba448a4430e
ryo ono <ryo.ono@gmail.com>
2023-05-31 17:28:48 +0900

    Add_seedsにサンプル物件

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+RealEstate.create(name: "sample_西武球場", rent:"100,000,000", address:"所沢市下山口", age:"30", note:"芝生席は無くなってしまいました")
+RealEstate.create(name: "sample_Tokyo Tower", rent:"50000", address:"Tokyo", age:"50", note:"So Big!!")
